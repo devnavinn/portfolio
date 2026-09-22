20c35ea3ac468e6a39b38239ce9f73dc92fc5a9f
Navin Kumar <navinkumar241999@gmail.com>
2026-09-22 16:21:04 +0530

    changes

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,13 +4,13 @@ import WhatsAppFloat from "@/components/whatsapp-float";
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-5xl mx-auto print:max-w-none">
+    <div className="max-w-4xl mx-auto print:max-w-none">
       <div className="min-h-screen flex">
         <div className="print:hidden">
           <MainNav />
         </div>
         <main className="grow overflow-hidden px-6 print:px-0">
-          <div className="w-full h-full max-w-[1072px] mx-auto flex flex-col print:max-w-none">
+          <div className="w-full h-full max-w-[1072px] mx-auto flex flex-col print:max-w-none max-md:mb-8">
             <div className="print:hidden">
               <Header />
             </div>
